44b77528becad6182315977868c0e6ee938551f4
Ezequias <ezequiasherrera99@gmail.com>
2025-09-10 07:56:23 -0400

    AI integration and testing with a product

---
+++ services/AIservice.js
@@ -0,0 +1,25 @@
+export const sendToAI = async (prompt) => {
+  fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-goog-api-key": "AIzaSyD-ESfl-Mgvz6wHmaGNGh9LUq80mZU5GfM",
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: prompt,
+              },
+            ],
+          },
+        ],
+      }),
+    }
+  )
+    .then((data) => data.json())
+    .then((data) => data.candidates?.[0]?.content?.parts?.[0]?.text);
+};
